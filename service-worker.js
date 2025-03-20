553cb2d6e6bebdb2df66dd47c5cb4d799b171c17
Dev <devaram@duck.com>
2025-03-20 17:33:27 +0530

    v2 fixes in cache names

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,11 @@
-var timestampNow = new Date().getTime();
-const CACHE_NAME = "ss-weight-calculator-v1"+ timestampNow;
+const CACHE_NAME = "ss-weight-calculator-v2";
 const ASSETS = [
     "./index.html",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css",
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css",
+    "https://code.jquery.com/jquery-3.6.0.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js",
     "https://cdn.jsdelivr.net/npm/vue@3.2.47"
 ];
 
